3e5be7f3886410c488e0783512ce299b9da9b6ae
hakktastic <hakktastic@gmail.com>
2023-02-19 19:09:21 +0100

    HAK-102 add find by productID & email
    - to use in order creation validation

--- src/main/java/nl/hakktastic/order/repository/OrderRepository.java
+++ src/main/java/nl/hakktastic/order/repository/OrderRepository.java
@@ -8,4 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  *
  */
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+
+    Order findByProductIDAndEmail(int productID, String email);
 }
