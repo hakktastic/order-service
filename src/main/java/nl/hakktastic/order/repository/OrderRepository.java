275f3679b23b0cce17afc4d3514d2369e41a1364
hakktastic <hakktastic@gmail.com>
2023-02-19 15:36:29 +0100

    HAK-102 add JpaRepository
    to interact with repository

---
+++ src/main/java/nl/hakktastic/order/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package nl.hakktastic.order.repository;
+
+import nl.hakktastic.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Order Repository.
+ *
+ */
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+}
