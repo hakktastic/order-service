70a102a9c9f4b222783abdf70caef4fd184cfb7f
hakktastic <hakktastic@gmail.com>
2023-02-19 19:14:01 +0100

    HAK-102 add exception handling for order creation
    - to handle exception if order already exists
    - to handle exception if unexpected error occurs while creating an order
    - to handle exception for validations

--- src/main/java/nl/hakktastic/order/exception/OrderExceptionHandler.java
+++ src/main/java/nl/hakktastic/order/exception/OrderExceptionHandler.java
@@ -1,6 +1,8 @@
 package nl.hakktastic.order.exception;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,7 +25,49 @@ public class OrderExceptionHandler {
                 .statusCode(HttpStatus.NOT_FOUND.value())
                 .timestamp(LocalDateTime.now())
                 .message(exception.getMessage())
-                .description(request.getDescription(true))
+                .description(request.getDescription(false))
+                .build();
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public OrderError methodArgumentNotValidException(MethodArgumentNotValidException exception, WebRequest request){
+
+        var errorList = exception.getBindingResult().getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+
+        return OrderError.builder()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDateTime.now())
+                .message(exception.getMessage())
+                .description(request.getDescription(false))
+                .errorList(errorList)
+                .build();
+    }
+
+    @ExceptionHandler({OrderNotCreatedException.class})
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public OrderError orderNotCreatedException(OrderNotCreatedException exception, WebRequest request){
+
+        return OrderError.builder()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDateTime.now())
+                .message(exception.getMessage())
+                .description(request.getDescription(false))
+                .build();
+    }
+
+    @ExceptionHandler({OrderAlreadyExistsException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public OrderError orderAlreadyExistsException(OrderAlreadyExistsException exception, WebRequest request){
+
+        return OrderError.builder()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDateTime.now())
+                .message(exception.getMessage())
+                .description(request.getDescription(false))
                 .build();
     }
 }
