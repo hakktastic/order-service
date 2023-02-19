998bb0f30a86faa74e82308172698eb94335bdc4
hakktastic <hakktastic@gmail.com>
2023-02-19 09:38:54 +0100

    HAK-102 add sql insert script for orders
    - to generate test data
    -
    -

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into product_order (orderID, email, first_name, last_name, productID) values (1,'george.bluth@reqres.in', 'George', 'Bluth', 34);
+insert into product_order (orderID, email, first_name, last_name, productID) values (2,'emma.wong@reqres.in', 'Emma', 'Wong', 20);
+insert into product_order (orderID, email, first_name, last_name, productID) values (3,'tracey.ramos@reqres.in', 'Tracey', 'Ramos', 25);
+insert into product_order (orderID, email, first_name, last_name, productID) values (4,'tobias.funke@reqres.in', 'Tobias', 'Funk', 38);
+insert into product_order (orderID, email, first_name, last_name, productID) values (5,'rachel.howell@reqres.in', 'Rachel', 'Howell', 41);
