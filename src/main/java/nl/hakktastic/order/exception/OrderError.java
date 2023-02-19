a4318b55d251feff1d7ad5264107c292208ae446
hakktastic <hakktastic@gmail.com>
2023-02-19 15:37:48 +0100

    HAK-102 add generic error message
    to provide a consistent response for all requests

---
+++ src/main/java/nl/hakktastic/order/exception/OrderError.java
@@ -0,0 +1,27 @@
+package nl.hakktastic.order.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Custom Error object to provide a consistent response.
+ */
+@Slf4j
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderError {
+
+    private int statusCode;
+    private LocalDateTime timestamp;
+    private String message;
+    private String description;
+    private List<String> errorList;
+}
