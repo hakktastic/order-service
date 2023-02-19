76685cf36675cf54a767a191c895d097c1794ed7
hakktastic <hakktastic@gmail.com>
2023-02-19 15:32:14 +0100

    HAK-102 add sql test data generation
    to provide test data when performing integration tests

---
+++ src/test/resources/import.sql
@@ -0,0 +1,5 @@
+insert into product_order (orderID, email, first_name, last_name, productID) values (100,'george.bluth@reqres.in', 'George', 'Bluth', 34);
+insert into product_order (orderID, email, first_name, last_name, productID) values (101,'emma.wong@reqres.in', 'Emma', 'Wong', 20);
+insert into product_order (orderID, email, first_name, last_name, productID) values (102,'tracey.ramos@reqres.in', 'Tracey', 'Ramos', 25);
+insert into product_order (orderID, email, first_name, last_name, productID) values (103,'tobias.funke@reqres.in', 'Tobias', 'Funk', 38);
+insert into product_order (orderID, email, first_name, last_name, productID) values (104,'rachel.howell@reqres.in', 'Rachel', 'Howell', 41);
