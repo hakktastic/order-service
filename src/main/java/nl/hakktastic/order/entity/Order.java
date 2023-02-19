71f90adfdfc0f2378976100469b662a4ad54bd5e
hakktastic <hakktastic@gmail.com>
2023-02-19 09:37:22 +0100

    HAK-102 add order entity
    - to persist orders

---
+++ src/main/java/nl/hakktastic/order/entity/Order.java
@@ -0,0 +1,48 @@
+package nl.hakktastic.order.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+/**
+ * JPA Order Entity.
+ */
+@Slf4j
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Validated
+@Entity
+@Table(name = "product_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int orderID;
+
+    @Positive(message = "Product ID should be positive")
+    private int productID;
+
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank
+    @NotBlank(message = "Last name should be provided")
+    @Size(max = 256, message = "First name should be max. 256 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name should be provided")
+    @Size(max = 256, message = "Last name should be max. 256 characters")
+    private String lastName;
+}
