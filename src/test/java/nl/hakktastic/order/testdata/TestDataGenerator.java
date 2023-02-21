5e17fbb1bf3c24db999390bece367032daaa1747
hakktastic <hakktastic@gmail.com>
2023-02-21 08:38:51 +0100

    HAK-102 add mock data generation for mockWebServer
    - to use as response for mocking the webclient

--- src/test/java/nl/hakktastic/order/testdata/TestDataGenerator.java
+++ src/test/java/nl/hakktastic/order/testdata/TestDataGenerator.java
@@ -1,5 +1,7 @@
 package nl.hakktastic.order.testdata;
 
+import nl.hakktastic.order.client.response.Data;
+import nl.hakktastic.order.client.response.Response;
 import nl.hakktastic.order.entity.Order;
 
 import java.util.List;
@@ -40,4 +42,47 @@ public class TestDataGenerator {
 
         return List.of(order1,order2, order3);
     }
+
+    /**
+     * Get mock order.
+     *
+     * @return a mock order
+     */
+    public static Order getMockOrder(){
+
+        return Order
+                .builder()
+                .email("test.user@reqres.in")
+                .firstName("test")
+                .lastName("user")
+                .productID(1)
+                .build();
+    }
+
+    /**
+     * Get a mock response.
+     *
+     * @return a mock response
+     */
+    public static Response getMockResponse(){
+
+        var mockData = Data
+                .builder()
+                .id(1)
+                .email("test.user@reqres.in")
+                .first_name("test")
+                .last_name("user")
+                .avatar("https://reqres.in/img/faces/x-image.jpg")
+                .build();
+
+        return Response
+                .builder()
+                .page(1)
+                .per_page(1)
+                .total(1)
+                .total_pages(1)
+                .data(List.of(mockData))
+                .build();
+    }
+
 }
